b2927a0a8a547283096e155f84f1ee0dbb82a73d
Sumit Kumar Jethani Jethani <sumitkumar_jethani@hotmail.com>
2023-02-10 12:42:15 +0100

    [DEV-1821] config file update

--- people_also_ask/config.py
+++ people_also_ask/config.py
@@ -6,11 +6,6 @@ load_dotenv()
 
 MAX_QUESTIONS = int(os.getenv('MAX_QUESTIONS', '10'))
 
-SCRAPPER_USER_AGENT = os.getenv(
-    'SCRAPPER_USER_AGENT',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-)
-
 try:
     SCRAPPER_HTTP_SERP_PROXY_AGENTS = json.loads(os.getenv("SCRAPPER_HTTP_SERP_PROXY_AGENTS", "[]"))
 except:
