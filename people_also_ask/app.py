02c17cf1dd91be8c885595fbaaee63e41b41f07b
Jose Domingo Garcia Torrejon <jgarcia@keytrends.ai>
2023-10-25 13:28:52 +0200

    fix imports (?)

--- people_also_ask/app.py
+++ people_also_ask/app.py
@@ -1,24 +1,22 @@
-import json
-import logging
 import sys
-
-import connexion
-from config import (PAA_CHANNEL_TIMEOUT, PAA_CLEANUP_INTERVAL,
-                    PAA_MAX_QUESTIONS, PAA_THREADS)
-from google import get_related_questions
-from logger.logger import configure_logging
-from paste.translogger import TransLogger
-from waitress import serve
-
 # setting path
 sys.path.append('./')
 
+import connexion
+import logging
+import json
+import config
+
+from waitress import serve
+from paste.translogger import TransLogger
+from logger.logger import configure_logging
+from google import get_related_questions
 
 logger = logging.getLogger('app')
 
 
 def paa(data):
-    return json.dumps(get_related_questions(data['keyword'], data['hl'], data['gl'], PAA_MAX_QUESTIONS))
+    return json.dumps(get_related_questions(data['keyword'], data['hl'], data['gl'], config.PAA_MAX_QUESTIONS))
 
 
 if __name__ == '__main__':
@@ -26,5 +24,5 @@ if __name__ == '__main__':
 
     app = connexion.FlaskApp(__name__, port=8080, specification_dir='swagger/', debug=True)
     app.add_api('api.yaml')
-    serve(TransLogger(app, setup_console_handler=False), host='0.0.0.0', port=8080, threads=PAA_THREADS,
-          channel_timeout=PAA_CHANNEL_TIMEOUT, cleanup_interval=PAA_CLEANUP_INTERVAL)
+    serve(TransLogger(app, setup_console_handler=False), host='0.0.0.0', port=8080, threads=config.PAA_THREADS,
+          channel_timeout=config.PAA_CHANNEL_TIMEOUT, cleanup_interval=config.PAA_CLEANUP_INTERVAL)
